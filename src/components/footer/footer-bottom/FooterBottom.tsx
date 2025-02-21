2c15bb2b97742fa2b9259d83f86dbe51fbbdfd4e
Martin6175903 <martin6175903@yandex.ru>
2025-02-21 22:59:19 +0300

    Added logic hover effect on an element

--- src/components/footer/footer-bottom/FooterBottom.tsx
+++ src/components/footer/footer-bottom/FooterBottom.tsx
@@ -1,35 +1,20 @@
 import SVGHandler from '@/utilities/SVGHandler.tsx';
 import HeartIcon from '@public/icons/heart.svg?react';
-import { useState } from 'react';
 
 const FooterBottom = () => {
-  const [hoverElem, setHoverElem] = useState(false);
-
-  const handleOverMouse = () => {
-    setHoverElem(true);
-    console.log(true);
-  }
-
-  const handleOutMouse = () => {
-    setHoverElem(false);
-    console.log(false);
-  }
 
   return (
     <div className={'py-5 bg-[rgb(41,44,55)]'}>
       <div className="container">
         <div className={'flex items-center justify-between'}>
-          <div className={'flex gap-0.5 group/edit'} onMouseEnter={handleOverMouse} onMouseLeave={handleOutMouse}>
+          <div className={'flex gap-0.5 group/edit'}>
             <div>
               &copy; All rights reserved.
             </div>
             <div className={'flex gap-1 items-center'}>
               <p>Made with</p>
               <span>
-                {hoverElem
-                  ? <SVGHandler hoverBG={'red-300'}><HeartIcon /></SVGHandler>
-                  : <SVGHandler><HeartIcon /></SVGHandler>
-                }
+                <SVGHandler hoverBG={'red-300'}><HeartIcon /></SVGHandler>
               </span>
               <p>by Createx Studio</p>
             </div>
